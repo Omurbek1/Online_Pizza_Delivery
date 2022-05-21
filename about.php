81d1a4dab6f89987efab494a4ce6de20c93cb1fe
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-21 13:54:12 +0600

    OnlinePizzaDelivery

--- about.php
+++ about.php
@@ -59,7 +59,7 @@
             <div class="carousel-background"><img src="assets/img/slide/slide-3.jpg" alt=""></div>
             <div class="carousel-container">
               <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown mb-0">Parmar Darshan Kiritbhai</h2><p>CE084 <a href="https://github.com/darshankparmar" target="_blank">@darshankparmar</a></p>
+                <h2 class="animate__animated animate__fadeInDown mb-0">Omurbek Mamytbekov</h2><p>CE084 <a href="https://github.com/darshankparmar" target="_blank">omurbekmamytbekov545@gmail.com</a></p>
                 <a href="index.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Get Started</a>
               </div>
             </div>
--- viewCart.php
+++ viewCart.php
@@ -99,14 +99,14 @@
                     <div class="pt-4 border bg-light rounded p-3">
                         <h5 class="mb-3 text-uppercase font-weight-bold text-center">Order summary</h5>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0 bg-light">Total Price<span>Rs. <?php echo $totalPrice ?></span></li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center px-0 bg-light">Shipping<span>Rs. 0</span></li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0 bg-light">Total Price<span> <?php echo $totalPrice ?> <span></span>KGZ</span></li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center px-0 bg-light">Shipping<span> 0</span>KGZ</li>
                             <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3 bg-light">
                                 <div>
                                     <strong>The total amount of</strong>
                                     <strong><p class="mb-0">(including Tax & Charge)</p></strong>
                                 </div>
-                                <span><strong>Rs. <?php echo $totalPrice ?></strong></span>
+                                <span><strong><?php echo $totalPrice ?></strong>KGZ</span>
                             </li>
                         </ul>
                         <div class="form-check">
