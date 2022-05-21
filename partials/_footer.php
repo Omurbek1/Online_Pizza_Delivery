fb63fba303e852e7fb8e340ecd5ef9acd1c451b2
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-21 13:28:13 +0600

    OnlinePizzaDelivery

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="https://github.com/Omurbek1" target="_blank" rel="noopener noreferrer">omurbekmamytbekov545@gmail.com</a></p>
+</div>
